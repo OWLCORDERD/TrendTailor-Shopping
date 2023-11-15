d8d282ba5f4dc25f749bca50112d35a42a29e7d0
MIYEOK <lim5570@naver.com>
2023-11-15 23:45:03 +0900

    11.15 Fixed : vercel 배포 웹 서버와 google search console 연동을 위한 metadata verification google 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@ import "./globals.scss";
 import type { Metadata } from "next";
 import { ThemeProvider } from "../../context/ThemeContext";
 import AuthSession from "component/AuthSession";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "WISH | SHOPPING SITE",
   description:
     "Wearing Interface Shopping Homepage, 트렌드 의류 정보를 공유하고 추천하며 다양한 의류를 판매하는 위시 의류 사이트입니다.",
   keywords: "옷, 트렌드, 의류, 쇼핑, 상점, 유튜브, 의류 트렌드, 스타일, 유행",
+  verification: {
+    google: "Y46ulKgr26amG4IdAHROTHgXqKB2TH_SF7iHftuFkH0",
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <AuthSession>
-      <html lang='en'>
+      <html lang='ko'>
         <body>
           <ThemeProvider>{children}</ThemeProvider>
         </body>
