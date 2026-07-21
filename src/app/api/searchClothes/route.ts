bde81a3a9005cd566a2853bc7b9b2118b32ff318
OWLCORDERD <lim5570@naver.com>
2026-07-21 19:01:33 +0900

    07.21 Fixed. 빌드 오류 개선

--- src/app/api/searchClothes/route.ts
+++ src/app/api/searchClothes/route.ts
@@ -1,4 +1,4 @@
-import { db } from "@/lib/firebase";
+import { db } from "@/shared/lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
 
